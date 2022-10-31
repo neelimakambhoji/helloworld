a2967fbfae40f73a5f0b47f869579747047a9ed0
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-10-31 17:36:27 -0500

    assignment1 initial

--- src/assgnments/assignments1/Math.java
+++ src/assgnments/assignments1/Math.java
@@ -1,4 +1,30 @@
 package assgnments.assignments1;
 
 public class Math {
+
+    // add function with integer parameters
+    public int add(int num1, int num2) {
+        int total = num1 + num2;
+        return total;
+    }
+
+
+    // subtract function with integer parameters
+    public int subtract(int num1, int num2) {
+        int total = num1 - num2;
+        return total;
+    }
+
+    // multiply function with integer parameters
+
+
+    // divide function with integer parameters
+
+    // modulo function with integer parameters
+
+
+    // print method without returning
+    public void print(int num1, int num2, int total) {
+        System.out.println(num1 + " " + num2 + " " + total);
+    }
 }
--- src/assgnments/assignments1/MathMain.java
+++ src/assgnments/assignments1/MathMain.java
@@ -1,4 +1,16 @@
 package assgnments.assignments1;
 
 public class MathMain {
+
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        Math math = new Math();
+        int addTotal = math.add(num1, num2); // it is returning, it is function
+        math.print(num1, num2, addTotal); // it won't return anything, but do something
+
+        // subtract method
+        int subtotal = math.subtract(num1, num2);
+        math.print(num1, num2, subtotal);
+    }
 }
