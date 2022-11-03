482826ead96c0c131725823f6988499303a3f028
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-11-03 05:12:49 -0500

    assignment1 initial

--- src/assgnments/assignments1/Math.java
+++ src/assgnments/assignments1/Math.java
@@ -6,36 +6,59 @@ public class Math {
     public int add(int num1, int num2) {
         int total = num1 + num2;
         return total;
-    }
+
+        }
+
+    //Method OverLoad
+    public void add(float num1 float num2) {
+        float total = num1 + num2;
+        return num1 + num2 ;
+
+            Math.math = new Math();
+            System.out.print(num1f, num2f);
+        }
 
 
+     //add  function with float parameters
+       public float add(float num1, float num2){
+        float total = num1 + num2;
+        return total;
+       }
     // subtract function with integer parameters
     public int subtract(int num1, int num2) {
         int total = num1 - num2;
         return total;
     }
 
-    // multiply function with integer parameters
-      public int Multiply(int num1, int num2){
-        int total = num1 * num2;
-        return total;
-      }
+
+    // subtract function with float parameters
+      public float subtract(float num1, float num2) {
+          float total = num1 - num2;
+          return total;
+          // multiply function with integer parameters
+          public int Multiply (int num1, int num2 ){
+              int total = num1 * num2;
+              return total;
+          }
 
 
-    // divide function with integer parameters
-       public int Divide(int num1,int num2){
-        int total = num1 / num2 ;
-        return total;
-       }
-    // modulo function with integer parameters
-    public int Modulo(int num1,int num2) {
-        int total = num1 / num2;
-        return total;
-    }
-    // print method without returning
-    public void print(int num1, int num2, int total) {
-        System.out.println(num1 + " " + num2 + " " + total);
-    }
+          // divide function with integer parameters
+          public int Divide ( int num1, int num2){
+              int total = num1 / num2;
+              return total;
+          }
+          // modulo function with integer parameters
+          public int Modulo ( int num1, int num2){
+              int total = num1 / num2;
+              return total;
+          }
+          // print method without returning
+          public void print ( int num1, int num2, int total){
+              System.out.println(num1 + " " + num2 + " " + total);
+              public void print( float num1, float num2 float total){
+                  System.out.println(num1 + " " + num2 + " " + total);
+                  return total;
+              }
 
-    }
+          }
 
--- src/assgnments/assignments1/MathMain.java
+++ src/assgnments/assignments1/MathMain.java
@@ -8,21 +8,24 @@ public class MathMain {
         Math math = new Math();
         int addTotal = math.add(num1, num2); // it is returning, it is function
         math.print(num1, num2, addTotal); // it won't return anything, but do something
-
+        float  Total = math.add(num1, num2); // it is returning, it is function
+        math.print(num1, num2, addTotal);
+          System.out.println (add(a,b,2));
         // subtract method
         int subtotal = math.subtract(num1, num2);
         math.print(num1, num2, subtotal);
 
         //Multiplication method
-        int Multiplytotal = math.Multiply(num1, num2);
+        int Multiply = math.Multiply(num1, num2);
         math.print(num1, num2, Multiplytotal);
 
         // Divide method
-        int dividetotal = math.Divide(num1, num2);
+        int Divide = math.Divide(num1, num2);
         math.print(num1, num2, dividetotal);
 
         //modulo method
-        int modulototal = math.Modulo(num1,num2);
+        int Modulo = math.Modulo(num1,num2);
         math.print(num1, num2, modulototal);
+
     }
 }
