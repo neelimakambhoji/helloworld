e2158ee9f311bf495aef707c20a1a275ff55bee4
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-11-13 18:53:59 -0600

    assignment1 initial

---
+++ src/assignment2/Employee.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int id;
+    private double salary;
+    private double issalarised;
+
+}
