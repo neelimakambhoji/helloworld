a645205d21d883e1d19b2b09bfe4652f21ee0070
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-11-13 19:02:06 -0600

    assignment1 initial

--- src/assgnments/assignments1/Math.java
+++ src/assgnments/assignments1/Math.java
@@ -20,63 +20,63 @@ public class Math {
      }
     // subtract function with integer parameters
     public int subtract(int num1, int num2) {
-        int Total = num1 - num2;
-        return Total;
+        int total = num1 - num2;
+        return total;
     }
 
     // subtract function with float parameters
       public float subtract(float num1, float num2) {
-          float Total = num1 - num2;
-               return Total;
+          float total = num1 - num2;
+               return total;
       }
       //subtract function with double parameters
        public double subtract(double num1, double num2){
-           double Total = num1 - num2;
-                return Total;
+           double total = num1 - num2;
+                return total;
     }
 
       // multiply function with integer parameters
           public int multiply(int num1, int num2){
 
-              int Total = num1 * num2;
-                    return Total;
+              int total = num1 * num2;
+                    return total;
           }
          public float multiply(float num1, float num2) {
-             float Total = num1 * num2;
-             return Total;
+             float total = num1 * num2;
+             return total;
          }
        // multiply function with double parameters
           public double multiply(double num1, double num2){
-               double Total = num1 * num2;
-                  return Total;
+               double total = num1 * num2;
+                  return total;
           }
           // divide function with integer parameters
           public int divide(int num1, int num2){
-              int Total = num1 / num2;
-              return Total;
+              int total = num1 / num2;
+              return total;
           }
           public float divide(float num1, float num2){
-                  float Total = num1 / num2;
-                       return Total;
+                  float total = num1 / num2;
+                       return total;
           }
           //divide function with double parameters
           public double divide(double num1,double num2){
-                    double Total = num1 / num2;
-                    return Total;
+                    double total = num1 / num2;
+                    return total;
           }
           // modulo function with integer parameters
           public int modulo(int num1, int num2){
-              int Total = num1 % num2;
-                   return Total;
+              int total = num1 % num2;
+                   return total;
           }
           public float modulo(float num1, float num2){
-                float Total = num1 % num2;
-                    return Total;
+                float total = num1 % num2;
+                    return total;
           }
           // modulo function with double parameters
             public double modulo(double num1, double num2){
-              double Total = num1 % num2;
-                 return Total;
+              double total = num1 % num2;
+                 return total;
             }
           // print method without returning
           public void print(int num1, int num2, int total) {
