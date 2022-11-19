3ef65ed268302149fcaa970b9e990639fa331441
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-11-19 06:09:53 -0600

    assignment1 initial

---
+++ src/assgnments/assignment2/Phone1.java
@@ -0,0 +1,8 @@
+package assgnments.assignment2;
+
+public class Phone1 {
+    public static void main(String[]args){
+        Phone phone = new Phone();
+        System.out.println(phone);
+    }
+}
