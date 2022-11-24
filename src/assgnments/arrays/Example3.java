58b2896780481549e053c261787e9af4c5634eff
neelimakambhoji <neelima.kambhoji@gmail.com>
2022-11-23 22:17:59 -0600

    assignment1 initial

--- src/assgnments/arrays/Example3.java
+++ src/assgnments/arrays/Example3.java
@@ -1,28 +1,32 @@
 package assgnments.arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Example3 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter the array size:");
+         int arrSize = scanner.nextInt();
+         int[]arr = new int[ arrSize];
+         System.out.println("please enter the array values:");
+         for(int i=0; i < arrSize; i++){
+             arr [i] = scanner.nextInt();
+         }
 
-
-         int[]a ={2,5,1,9,6};   //n-5   n-1 =4
+         System.out.println("input :" +Arrays.toString(arr));
+        // int[]a ={2,5,1,9,6};   //n-5   n-1 =4
          int sum =0;
+         // int lengthOfArray = numbers.length;
+       for(int i=0; i<arr.length;i++){       //0 1 2 3 4  //a.length=5
 
-
-
-       // int lengthOfArray = numbers.length;
-       for(int i=0; i<=a.length-1;i++){
-
-      //0 1 2 3 4  //a.length=5
-        sum = sum + a[i]; //2 + 5 + 1 + 9 + 6
+           sum = sum + arr[i]; //2 + 5 + 1 + 9 + 6
     }
-        //Enhanced for loop
-       // for(int value:a){
-       //     sum = sum +value;
-       // }
-            System.out.println("Sum of array elements: "+ sum);
+
+            System.out.println("output: "+ sum);
 
 
-
+     scanner.close();
 
     }
     }
